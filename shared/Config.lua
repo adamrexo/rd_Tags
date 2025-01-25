33e74974e1d96860cf6c7fa7bdeb8581c2e6211e
adamrexo <127039714+adamrexo@users.noreply.github.com>
2025-01-25 12:45:06 +0100

    Updated config.lua (Added Staff Tags)

--- shared/Config.lua
+++ shared/Config.lua
@@ -31,14 +31,45 @@ Config.Tags = {
             Sign = "bzzz_player_sign_owner" -- Prop Name (You can add other prop than default.)
         },
 
-        ["Admin"] = { -- Group Name
-            Sign = "bzzz_player_sign_admin" -- Prop Name (You can add other prop than default.)
+        ["Management"] = { -- Group Name
+            Sign = "bzzz_player_sign_management" -- Prop Name (You can add other prop than default.)
+        },
+
+        ["Head Developer"] = { -- Group Name
+            Sign = "bzzz_player_sign_headdeveloper" -- Prop Name (You can add other prop than default.)
+        },
+
+        ["Developer"] = { -- Group Name
+            Sign = "bzzz_player_sign_developer" -- Prop Name (You can add other prop than default.)
+        },
+
+        ["Trial Developer"] = { -- Group Name
+            Sign = "bzzz_player_sign_trialdeveloper" -- Prop Name (You can add other prop than default.)
+        },
+
+        ["Head of Staff"] = { -- Group Name
+            Sign = "bzzz_player_sign_headofstaff" -- Prop Name (You can add other prop than default.)
+        },
+
+        ["Senior Staff"] = { -- Group Name
+            Sign = "bzzz_player_sign_seniorstaff" -- Prop Name (You can add other prop than default.)
         },
 
         ["Staff"] = { -- Group Name
             Sign = "bzzz_player_sign_staff" -- Prop Name (You can add other prop than default.)
         },  
         
+        ["Admin"] = { -- Group Name
+            Sign = "bzzz_player_sign_admin" -- Prop Name (You can add other prop than default.)
+        },
+
+        ["Mod"] = { -- Group Name
+            Sign = "bzzz_player_sign_mod" -- Prop Name (You can add other prop than default.)
+        },
+
+        ["Trial Mod"] = { -- Group Name
+            Sign = "bzzz_player_sign_trialmod" -- Prop Name (You can add other prop than default.)
+        },
     },
 
     Command = {
